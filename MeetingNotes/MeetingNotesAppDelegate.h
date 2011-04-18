04c116e61334545441cd9c6bd4f860992b69a6db
Noel Curtis <noelcurtis@gmail.com>
2011-04-18 18:59:03 -0400

    Initial Commit

---
+++ MeetingNotes/MeetingNotesAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  MeetingNotesAppDelegate.h
+//  MeetingNotes
+//
+//  Created by Noel Curtis on 4/18/11.
+//  Copyright 2011 EMC Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeetingNotesAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ MeetingNotes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MeetingNotes
+//
+//  Created by Noel Curtis on 4/18/11.
+//  Copyright 2011 EMC Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
