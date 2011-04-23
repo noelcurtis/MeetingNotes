4d6a33b1eb5c913bf9b6a5948a5e4919fd6a2d98
Noel Curtis <noelcurtis@gmail.com>
2011-04-23 10:49:38 -0400

    Adding support for core-data to the project

--- MeetingNotes/MeetingNotesAppDelegate.h
+++ MeetingNotes/MeetingNotesAppDelegate.h
@@ -12,12 +12,20 @@
 @class SortDetailViewController;
 
 @interface MeetingNotesAppDelegate : NSObject <UIApplicationDelegate> {
-
+    
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) SortRootViewController *sortRVController;
 @property (nonatomic, retain) SortDetailViewController *sortDVController;
 @property (nonatomic, retain) UISplitViewController *splitViewController;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
 
 @end
