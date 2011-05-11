752637743119bfd11aa8c1b3814fa6f65cbf68d7
Noel Curtis <noelcurtis@gmail.com>
2011-05-10 20:50:04 -0400

    Update to add action items UI to the app.

---
+++ MeetingNotes/ActionItemsViewController.h
@@ -0,0 +1,16 @@
+//
+//  ActionItemsViewController.h
+//  MeetingNotes
+//
+//  Created by Noel Curtis on 5/8/11.
+//  Copyright 2011 EMC Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ActionItemsViewController : UITableViewController {
+    
+}
+
+@end
